34e6ee6a42b92d2f3048daf63979b57fa0e8082e
Calum Knott <calumk@mac.uni.mdx.ac.uk>
2017-12-14 15:29:57 +0000

    first commit of application to github

--- renderer.js
+++ renderer.js
@@ -1,3 +1,25 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+
+function sayHello() {
+    alert(document.getElementById("myDiv").innerHTML)
+}
+
+function changeMessage(message) {
+    document.getElementById("myDiv").innerHTML = message
+}
+
+
+var count = 0;
+function updateCounter() {
+    document.getElementById("counter").innerHTML = "<br>" + count;
+    count = count + 1;
+}
+
+var button = document.getElementById("button");
+button.onclick = function () { 
+    updateCounter()
+ }
+
